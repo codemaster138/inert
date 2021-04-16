8b1f209333c28a380782c6d2870cb8273eb27376
codemaster138 <pygamer138@gmail.com>
2021-04-16 11:27:19 +0200

    feat: moar debugs

--- src/core/build/user-utils/copy.ts
+++ src/core/build/user-utils/copy.ts
@@ -2,12 +2,16 @@ import { InertConfig, InertFile } from "../types";
 import { promises as fsPromises } from "fs";
 import { resolve } from "path";
 import { resolveOutDir } from "../utils/dirs";
+import { cyan } from "chalk";
 
 /**
  * Create a build middleware that copies files from their original location into the target directory
  */
 export default function copy(destination: string) {
   return async (config: InertConfig, file: InertFile, previous: any) => {
+    config.custom?.spinner?.stop();
+    config.custom?.log?.verb(`Copying file ${cyan(file.relative)}`);
+    config.custom?.spinner?.start();
     const _destination = resolve(process.cwd(), resolveOutDir(config, config.build.outDirs[destination]), file.relative);
     await fsPromises.copyFile(file.path, _destination);
     return previous;
