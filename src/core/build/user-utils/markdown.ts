94f4ea9cc605cf43838e1c415985845a1c563245
codemaster138 <pygamer138@gmail.com>
2021-05-03 11:19:42 +0200

    feat: add support for custom plugins

--- src/core/build/user-utils/markdown.ts
+++ src/core/build/user-utils/markdown.ts
@@ -1,7 +1,7 @@
 import { InertConfig, InertFile } from "../types";
 import { readFileSync } from "fs";
 import MarkdownIt from "markdown-it";
-import Prism from "prismjs";
+import Prism, { plugins } from "prismjs";
 import fm from "front-matter";
 import { cyan } from "chalk";
 import { getOptimizedImage } from ".";
@@ -13,7 +13,15 @@ require("prismjs/components/index")();
 /**
  * Return a build utility that compiles markdown to html
  */
-export default function markdown(markdownItOptions?: MarkdownIt.Options) {
+export default function markdown(
+  markdownItOptions?: MarkdownIt.Options & {
+    plugins: (
+      | MarkdownIt.PluginSimple
+      | MarkdownIt.PluginWithOptions
+      | MarkdownIt.PluginWithParams
+    )[];
+  }
+) {
   return (config: InertConfig, file: InertFile, previous: any) => {
     const default_options: MarkdownIt.Options = {
       highlight: (str: string, lang: string) => {
@@ -37,13 +45,23 @@ export default function markdown(markdownItOptions?: MarkdownIt.Options) {
 
     compiler.use(markdownItNamedHeadings);
     compiler.use(markdownItFootnote);
+    markdownItOptions?.plugins.forEach((plugin) => compiler.use(plugin));
 
-    const defaultImage = compiler.renderer.rules.image || (() => '');
-    compiler.renderer.rules.image = (tokens, idx, options, env, self): string => {
+    const defaultImage = compiler.renderer.rules.image || (() => "");
+    compiler.renderer.rules.image = (
+      tokens,
+      idx,
+      options,
+      env,
+      self
+    ): string => {
       const token = tokens[idx];
-      const srcIdx = token.attrIndex('src');
+      const srcIdx = token.attrIndex("src");
 
-      return getOptimizedImage(config, token.attrs?.[srcIdx][1] || '') || defaultImage(tokens, idx, options, env, self);
+      return (
+        getOptimizedImage(config, token.attrs?.[srcIdx][1] || "") ||
+        defaultImage(tokens, idx, options, env, self)
+      );
     };
 
     const spinner_text = config.custom?.spinner?.text;
