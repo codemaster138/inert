e3d89d22e707fb48719d81499890162c146f1976
codemaster138 <pygamer138@gmail.com>
2021-05-21 18:57:18 +0200

    fix: read alt from children, not attribute

--- src/core/build/user-utils/markdown.ts
+++ src/core/build/user-utils/markdown.ts
@@ -57,10 +57,9 @@ export default function markdown(
     ): string => {
       const token = tokens[idx];
       const srcIdx = token.attrIndex("src");
-      const altIdx = token.attrIndex("alt");
 
       return (
-        getOptimizedImage(config, token.attrs?.[srcIdx][1] || "", token.attrs?.[altIdx]?.[1]) ||
+        getOptimizedImage(config, token.attrs?.[srcIdx][1] || "", token.children?.[0].content.toString()) ||
         defaultImage(tokens, idx, options, env, self)
       );
     };
